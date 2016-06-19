6d0fbe144713b8beceea04a88eccbe46b6f3a46c
agrippa1994 <agrippa1994@icloud.com>
2016-06-19 21:26:52 +0200

    SVG can now be generated

--- index.js
+++ index.js
@@ -86,6 +86,51 @@ app.get("/builds", (req, res) => {
 	);
 });
 
+app.get("/status.svg", (req, res) => {
+	db.build.findAll({ order: [["id", "DESC"]] }).then(
+		(rows) => {
+			if(rows.length == 0)
+				return res.status(500).end();
+
+			const build = rows[0];
+			var color = "#4c1";
+			var text = "passing";
+
+			if(build.code == 0) {
+				if(build.stderr.length > 0) {
+					color = "#cc8111";
+					text = "warnings";
+				}
+			} else {
+				color = "#cc3611";
+				text = "failed";
+			}
+			
+			const svg = `
+				<svg xmlns="http://www.w3.org/2000/svg" width="90" height="20">
+					<linearGradient id="a" x2="0" y2="100%">
+						<stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+						<stop offset="1" stop-opacity=".1"/>
+					</linearGradient>
+					<rect rx="3" width="90" height="20" fill="#555"/>
+					<rect rx="3" x="37" width="53" height="20" fill="${color}"/>
+					<path fill="${color}" d="M37 0h4v20h-4z"/>
+					<rect rx="3" width="90" height="20" fill="url(#a)"/>
+					<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
+						<text x="19.5" y="15" fill="#010101" fill-opacity=".3">build</text>
+						<text x="19.5" y="14">build</text>
+						<text x="62.5" y="15" fill="#010101" fill-opacity=".3">${text}</text>
+						<text x="62.5" y="14">${text}</text>
+					</g>
+				</svg>
+			`;
+
+			res.set("Content-Type", "image/svg+xml").status(200).end(svg);
+		},
+		err => res.status(500).end()
+	);
+});
+
 app.get("*", (req, res) => {
     var options = {
         root: `${__dirname}/www/`,
